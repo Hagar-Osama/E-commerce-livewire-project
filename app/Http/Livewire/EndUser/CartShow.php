1e1e283607898a193e23c64e915d0bcc18a30d1e
Hagar Osama <hagarosama142@gmail.com>
2022-09-06 07:44:53 +0200

    cart quantity increment & decrement

--- app/Http/Livewire/EndUser/CartShow.php
+++ app/Http/Livewire/EndUser/CartShow.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\EndUser;
 
 use App\Models\Cart;
+use App\Models\ProductColor;
 use Livewire\Component;
 
 class CartShow extends Component
@@ -12,5 +13,97 @@ class CartShow extends Component
         $carts = Cart::where('user_id', auth()->user()->id)->get();
 
         return view('livewire.end-user.cart-show', ['carts' => $carts]);
+    }
+
+    public function incrementQty($cartId)
+    {
+        $cart = Cart::where([['user_id', auth()->user()->id], ['id', $cartId]])->first();
+        if ($cart) {
+            if (ProductColor::where('id', $cart->product_color_id)->exists()) {
+                $productColors = ProductColor::where('id', $cart->product_color_id)->first();
+                if ($productColors->color_qty > $cart->quantity) {
+                    $cart->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                } else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' . $productColors->color_qty . ' Available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            } else {
+                if ($cart->products->qty > $cart->quantity) {
+                    $cart->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                } else {
+
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' . $cart->products->qty . ' Available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+        } else {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'something went wrong',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
+
+    public function decrementQty($cartId)
+    {
+        $cart = Cart::where([['user_id', auth()->user()->id], ['id', $cartId]])->first();
+        if ($cart) {
+            if (ProductColor::where('id', $cart->product_color_id)->exists()) {
+                $productColors = ProductColor::where('id', $cart->product_color_id)->first();
+                if ($productColors->color_qty > $cart->quantity) {
+                    $cart->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                } else {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' . $productColors->color_qty . ' Available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            } else {
+                if ($cart->products->qty > $cart->quantity) {
+                    $cart->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                } else {
+
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' . $cart->products->qty . ' Available',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+            }
+        } else {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'something went wrong',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
     }
 }
